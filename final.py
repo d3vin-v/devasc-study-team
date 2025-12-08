3e7cd598cd3e47b290d1c42a0d55195c446c843e
d3vin-v <devinje01@gmail.com>
2025-12-08 17:59:22 +0000

    fully functional menu. compartmentalized route function

--- final.py
+++ final.py
@@ -33,7 +33,7 @@ def geocoding (location, key):
         
         if len(state) !=0 and len(country) !=0:
             new_loc = name + ", " + state + ", " + country
-        elif len(state) !=0:
+        elif len(country) !=0:
             new_loc = name + ", " + country
         else:
             new_loc = name
@@ -44,61 +44,45 @@ def geocoding (location, key):
         lng="null"
         new_loc=location
         if json_status != 200:
-            print("Deocode API status: " + str(json_status) + "\nError message: " + json_data["message"])
+            print("Geocode API status: " + str(json_status) + "\nError message: " + json_data["message"])
     return json_status,lat,lng,new_loc
 #geocoding function end
 
-#user input
-while True:
-    print("\n###############################################")
-    print("\nMenu")
-    print("1. Route finder")
-    print("2. Basic geocoding")
-    print("3. Quit")
-    menu = input("")
-    #menu
-    if menu == "1":
-        routing()
-    elif menu == "2":
-        geocoding()
-    elif menu == "3":
-        break
-    print("\n+++++++++++++++++++++++++++++++++++++++++++++++")
-    print("Vehicle profiles available on Graphhopper:")
-    print("+++++++++++++++++++++++++++++++++++++++++++++++")
-    print("car, bike, foot")
-    print("+++++++++++++++++++++++++++++++++++++++++++++++")
-    profile=["car", "bike", "foot"]
-    vehicle = input("Enter a vehicle profile from the list above: ")
-    if vehicle == "quit" or vehicle == "q":
-        break
-    elif vehicle in profile:
-        vehicle = vehicle
-    else:
-        vehicle = "car"
-        print("No valid vehicle profile was entered. Using the car profile.")
+def routing (vehicle):
     loc1 = input("Starting Location: ")
     if loc1 == "quit" or loc1 == "q":
-        break
+        return
     orig = geocoding(loc1, key)
+    #locations are checked for validity
+    if orig[0] != 200:
+        return
 
     loc2 = input("Destination: ")
     if loc2 == "quit" or loc2 == "q":
-        break
+        return
     dest = geocoding(loc2, key)
+    if dest[0] != 200:
+        return
+
     print("===========================================")
     if orig[0] == 200 and dest[0] == 200:
         op="&point="+str(orig[1])+","+str(orig[2])
         dp="&point="+str(dest[1])+","+str(dest[2])
         paths_url = route_url + urllib.parse.urlencode({"key":key, "vehicle":vehicle}) + op + dp
-        paths_status = requests.get(paths_url).status_code 
-        paths_data = requests.get(paths_url).json()
+        response = requests.get(paths_url)
+        paths_status = response.status_code
+        paths_data = response.json()
         print("Routing API Status: " + str(paths_status) + "\nRouting API URL:\n" + paths_url)
+    else:
+        print("Error message: " + paths_data["message"])
+        print("**************************************************")
+        return
+
     print("===========================================")
     print("Directions from " + orig[3] + " to " + dest[3] + " by " + vehicle)
     print("===========================================")
     if paths_status == 200:
-        miles = (paths_data["paths"][0]["distance"])/1000/1.65
+        miles = (paths_data["paths"][0]["distance"])/1000/1.60934
         km = (paths_data["paths"][0]["distance"])/1000
         sec = int(paths_data["paths"][0]["time"]/1000%60)
         min = int(paths_data["paths"][0]["time"]/1000/60%60)
@@ -109,8 +93,46 @@ while True:
         for each in range(len(paths_data["paths"][0]["instructions"])):
             path = paths_data["paths"][0]["instructions"][each]["text"]
             distance = paths_data["paths"][0]["instructions"][each]["distance"]
-            print("{0} ( {1:.1f} km / {2:.1f} miles)".format(path, distance/1000, distance/1000/1.61))
+            print("{0} ( {1:.1f} km / {2:.1f} miles)".format(path, distance/1000, distance/1000/1.60934))
         print("===========================================")
     else:
         print("Error message: " + paths_data["message"])
         print("**************************************************")
+
+    return
+
+#user input
+while True:
+    print("\n###############################################")
+    print("\nMenu")
+    print("1. Route finder")
+    print("2. Basic geocoding")
+    print("3. Quit")
+    menu = input("Enter you selection:")
+    #menu
+    if menu == "1":
+        print("\n+++++++++++++++++++++++++++++++++++++++++++++++")
+        print("Vehicle profiles available on Graphhopper:")
+        print("+++++++++++++++++++++++++++++++++++++++++++++++")
+        print("car, bike, foot")
+        print("+++++++++++++++++++++++++++++++++++++++++++++++")
+        profile=["car", "bike", "foot"]
+        vehicle = input("Enter a vehicle profile from the list above: ")
+        if vehicle == "quit" or vehicle == "q":
+            break
+        elif vehicle in profile:
+            vehicle = vehicle
+        else:
+            vehicle = "car"
+            print("No valid vehicle profile was entered. Using the car profile.")
+        routing(vehicle)
+    elif menu == "2":
+        loc1 = input("Enter location: ")
+        if loc1 == "quit" or loc1 == "q":
+            break
+        orig = geocoding(loc1, key)
+        if orig[0] != 200:
+            print("Invalid location.")
+            break
+    elif menu == "3":
+        break
